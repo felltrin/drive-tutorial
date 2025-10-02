faeab8d8a9c87b022a06172e4b785045e11fa105
Lucius Holzinger <luciholzinger@gmail.com>
2025-10-02 13:28:06 -0700

    Database added

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,17 +3,27 @@ import { drizzle } from "drizzle-orm/libsql";
 
 import { env } from "~/env";
 import * as schema from "./schema";
+import { createPool, type Pool } from "mysql2/promise";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+  conn: Pool | undefined;
 };
 
 export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+  globalForDb.conn ??
+  createPool({
+    host: env.SINGLESTORE_HOST,
+    password: env.SINGLESTORE_PASS,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle: 0,
+  });
+if (env.NODE_ENV !== "production") globalForDb.conn = client;
 
 export const db = drizzle(client, { schema });
