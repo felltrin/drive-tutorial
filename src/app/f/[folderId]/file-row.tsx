58672f0c36210febe9d48a9016f88ab49fc052f8
Lucius Holzinger <luciholzinger@gmail.com>
2025-10-04 14:08:37 -0700

    delete

--- src/app/f/[folderId]/file-row.tsx
+++ src/app/f/[folderId]/file-row.tsx
@@ -1,5 +1,7 @@
-import { Folder as FolderIcon, FileIcon } from "lucide-react";
+import { Folder as FolderIcon, FileIcon, Trash2Icon } from "lucide-react";
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import { deleteFile } from "~/server/actions";
 import type { files_table, folders_table } from "~/server/db/schema";
 
 export const FileRow = (props: { file: typeof files_table.$inferSelect }) => {
@@ -21,6 +23,15 @@ export const FileRow = (props: { file: typeof files_table.$inferSelect }) => {
         </div>
         <div className="col-span-3 text-gray-400">{"file"}</div>
         <div className="col-span-3 text-gray-400">{file.size}</div>
+        <div className="col-span-3 text-gray-400">
+          <Button
+            variant="ghost"
+            onClick={() => deleteFile(file.id)}
+            aria-label="Delete button"
+          >
+            <Trash2Icon size={20} />
+          </Button>
+        </div>
       </div>
     </li>
   );
