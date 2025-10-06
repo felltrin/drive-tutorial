378d52591c850df709719aab45c07d8a925f337b
Lucius Holzinger <luciholzinger@gmail.com>
2025-10-06 11:48:06 -0700

    Folder deletion when the folder only has files inside!

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -83,6 +83,7 @@ export async function deleteFolder(folderId: number) {
       ),
     );
 
+  // folder deletion with nothing inside
   if (
     (childrenFolders === undefined || childrenFolders.length === 0) &&
     (childrenFiles === undefined || childrenFiles.length === 0)
@@ -98,8 +99,52 @@ export async function deleteFolder(folderId: number) {
     return { success: true };
   }
 
-  console.log(childrenFiles);
-  console.log(childrenFolders);
+  // deleting folder if it only has files inside
+  if (childrenFolders === undefined || childrenFolders.length === 0) {
+    const fileIdsToDelete = [];
+    for (const file of childrenFiles) {
+      fileIdsToDelete.push(file.id);
+    }
+
+    for (const fileId of fileIdsToDelete) {
+      const [file] = await db
+        .select()
+        .from(files_table)
+        .where(
+          and(
+            eq(files_table.id, fileId),
+            eq(files_table.ownerId, session.userId),
+          ),
+        );
+
+      if (!file) {
+        return { error: "File not found" };
+      }
+
+      const utapiResult = await utApi.deleteFiles([
+        file.url.replace("https://utfs.io/f/", ""),
+      ]);
+      console.log(utapiResult);
+
+      const dbDeleteResult = await db
+        .delete(files_table)
+        .where(eq(files_table.id, fileId));
+      console.log(dbDeleteResult);
+    }
+    const dbDeleteResult = await db
+      .delete(folders_table)
+      .where(eq(folders_table.id, folderId));
+    console.log(dbDeleteResult);
+
+    const c = await cookies();
+
+    c.set("force-refresh", JSON.stringify(Math.random()));
+
+    return { success: true };
+  }
+
+  //   console.log(childrenFiles);
+  //   console.log(childrenFolders);
 
   // retreive folders that have the received folder as a parent, and their children
   // recursion
